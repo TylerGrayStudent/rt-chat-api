e95ad64e4a08bf63af3a35d4bc060ddf383a6e94
Tyler Gray <grayt@g.cofc.edu>
2022-04-22 19:35:04 -0400

    yaml

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/websockets';
 import { NestGateway } from '@nestjs/websockets/interfaces/nest-gateway.interface';
 import { Server } from 'http';
+import { Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 
 interface User {
@@ -43,20 +44,23 @@ export class ChatGateway implements NestGateway {
 
   @Bind(MessageBody(), ConnectedSocket())
   @SubscribeMessage('connection')
-  onConnect(@MessageBody() name: string, @ConnectedSocket() socket: any) {
+  onConnect(@MessageBody() name: string, @ConnectedSocket() socket: Socket) {
     if (this.users.find((x) => x.username === name)) {
       socket.emit('error', 'Username already taken');
       return;
     }
     console.log(`User connected: ${name}`);
-
+    console.log(socket.client);
     this.users.push({ id: socket.id, username: name });
     this.chat.addChat({ message: `${name} joined the chat`, user: socket.id });
   }
 
   @Bind(MessageBody(), ConnectedSocket())
   @SubscribeMessage('message')
-  async onChat(@MessageBody() message: string, @ConnectedSocket() socket: any) {
+  async onChat(
+    @MessageBody() message: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
     const user = this.users.find((x) => x.id === socket.id);
     this.chat.addChat({ message: message, user: user.username });
   }
