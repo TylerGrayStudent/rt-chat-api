acff295d9c29792da829cf5e33d5b35cc8cfe8bb
Tyler Gray <grayt@g.cofc.edu>
2022-04-22 19:58:57 -0400

    disable chat for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ChatModule],
+  //imports: [ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
